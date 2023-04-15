e89a39ef9155e2b7fa0e66881ebcc4715d4681b4
Jacqueline Apodaca <jacapodaca42@gmail.com>
2023-04-14 21:03:50 -0600

    completedwork.md

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const findMatching = (names, string) => {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase());    
+}
+
+
+
+function fuzzyMatch(names,string){
+    const driverMatch = [];
+    for (const drivers of names){
+        if(string=names){
+            driverMatch.push(drivers)
+        }
+    } return driverMatch
+}
